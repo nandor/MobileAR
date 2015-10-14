ea7cf26c24498a62e2b4e2c68a6d9a147489d938
Nandor Licker <licker.nandor@gmail.com>
2015-10-14 08:50:37 +0100

    Initial Commit

---
+++ MobileAR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MobileAR
+//
+//  Created by Nandor Licker on 10/14/15.
+//  Copyright © 2015 Imperial College London. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
